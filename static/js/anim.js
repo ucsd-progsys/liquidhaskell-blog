5574e328341f38bee3023f815efa4652ef78328a
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-01-15 20:27:36 -0800

    update page

---
+++ static/js/anim.js
@@ -0,0 +1,26 @@
+function swizzle(name){
+    $(function() {
+        var divName  = "#" + name;
+        var animName = "/static/img/" + name + ".gif";
+        var statName = "/static/img/" + name + ".png";
+        $(divName).hover(
+            function() {
+                $(this).attr("src", animName);
+            },
+            function() {
+                $(this).attr("src", statName);
+            }
+        );
+    });
+};
+
+var z0 = swizzle("splash-head");
+var z1 = swizzle("splash-unstutter");
+var z2 = swizzle("splash-vectorsum");
+var z3 = swizzle("splash-dotproduct");
+var z4 = swizzle("splash-fib");
+var z5 = swizzle("splash-merge");
+var z6 = swizzle("splash-ups");
+var z7 = swizzle("splash-insertsort");
+var z8 = swizzle("splash-assocthm");
+var z9 = swizzle("splash-assocpf");
